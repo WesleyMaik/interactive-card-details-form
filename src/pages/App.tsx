cb0ac0228642e8d54625d46d9d98749239c82062
WesleyMaik <wesleynamikaze404@gmail.com>
2023-04-11 17:27:39 -0400

    feat: adding home page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,44 @@
+//Modules
+import { background, backgroundMobile } from "@/utils/constants";
+
+//Components
+import { Stack, Text } from "@chakra-ui/react";
+import { Card } from "@/components/Card";
+import { CardForm } from "@/components/Forms/CardForm";
+
 export default function App() {
   return (
-    <></>
-  )
-}
+    <Stack
+      direction={["column", "row"]}
+      as="main"
+      width="full"
+      height="full"
+      justifyContent={[
+        "flex-start",
+        "center"
+      ]}
+      alignItems="center"
+      backgroundImage={[backgroundMobile, background]}
+      backgroundRepeat="no-repeat"
+      backgroundSize={[
+        "100% 15rem",
+        "33.5% 100%"
+      ]}
+      padding={8}
+      spacing={["0", "32"]}
+    >
+      <Card
+        name="Jane Appleseed"
+        cardNumber="0000000000000000"
+        cvv="000"
+        date={{
+          month: "00",
+          year: "00"
+        }}
+      />
+      <CardForm
+        maxWidth="380"
+      />
+    </Stack>
+  );
+};
