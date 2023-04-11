ee496d5822e54fcf24872546670203be8d24d0a9
WesleyMaik <wesleynamikaze404@gmail.com>
2023-04-11 19:43:35 -0400

    changing assets from public to src folder

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,9 @@
-import background from "public/assets/bg-main-desktop.png";
-import backgroundMobile from "public/assets/bg-main-mobile.png";
-import frontCard from "public/assets/bg-card-front.png";
-import backCard from "public/assets/bg-card-back.png";
-import completed from "public/assets/icon-complete.svg"
-import brand from "public/assets/card-logo.svg"
+import background from "@/assets/bg-main-desktop.png";
+import backgroundMobile from "@/assets/bg-main-mobile.png";
+import frontCard from "@/assets/bg-card-front.png";
+import backCard from "@/assets/bg-card-back.png";
+import completed from "@/assets/icon-complete.svg"
+import brand from "@/assets/card-logo.svg"
 
 const Constants = {
     background,
