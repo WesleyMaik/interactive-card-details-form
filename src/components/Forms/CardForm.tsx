55635ba64e69f23d19a4d96cdf2e5992d24ebace
WesleyMaik <wesleynamikaze404@gmail.com>
2023-04-11 17:49:01 -0400

    adding validation

--- src/components/Forms/CardForm.tsx
+++ src/components/Forms/CardForm.tsx
@@ -7,9 +7,41 @@ import {
     Text
 } from "@chakra-ui/react";
 
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+
+const cardFormSchema = z.object({
+    name: z.string().nonempty({ message: "Can't be blank" }),
+    number: z.string()
+        .nonempty({ message: "Can't be blank" })
+        .min(16, { message: "Must have at least 16 numbers" })
+        .max(16, { message: "Maximum 16 numbers" })
+        .regex(/(\d{4}\s){3}\d{4}/),
+    date: z.object({
+        month: z.string().nonempty({ message: "Can't be blank" }),
+        year: z.string().nonempty({ message: "Can't be blank" })
+    }),
+    cvc: z
+        .string()
+        .nonempty({ message: "Can't be blank" })
+        .min(3, "Min 3")
+        .max(4)
+});
+
+type CardFormSchema = z.infer<typeof cardFormSchema>;
+
 export interface CardFormProps extends StackProps { };
 
 export const CardForm = ({ ...props }: CardFormProps) => {
+    const { register, handleSubmit, formState: { errors } } = useForm<CardFormSchema>({
+        resolver: zodResolver(cardFormSchema)
+    });
+
+    const handleSubmitForm = (data: CardFormSchema) => {
+        return;
+    };
+
     return (
         <Stack
             as="form"
@@ -21,10 +53,14 @@ export const CardForm = ({ ...props }: CardFormProps) => {
             <Input
                 label="Cardholder Name"
                 placeholder="e.g Jane Appleseed"
+                error={errors.name?.message}
+                {...register("name")}
             />
             <Input
                 label="Card Number"
                 placeholder="e.g 1234 5678 9123 0000"
+                error={errors.number?.message}
+                {...register("number")}
             />
             <HStack
                 alignItems="center"
@@ -42,19 +78,27 @@ export const CardForm = ({ ...props }: CardFormProps) => {
                         <Input
                             placeholder="MM"
                             maxWidth={32}
+                            error={errors.date?.month?.message}
+                            {...register("date.month")}
                         />
                         <Input
                             placeholder="YY"
                             maxWidth={32}
+                            error={errors.date?.year?.message}
+                            {...register("date.year")}
                         />
                     </HStack>
                 </Stack>
                 <Input
                     label="CVC"
                     placeholder="e.g. 123"
+                    error={errors.cvc?.message}
+                    {...register("cvc")}
                 />
             </HStack>
-            <Button>Confirm</Button>
+            <Button
+                onClick={handleSubmit(handleSubmitForm)}
+            >Confirm</Button>
         </Stack>
     );
 };
